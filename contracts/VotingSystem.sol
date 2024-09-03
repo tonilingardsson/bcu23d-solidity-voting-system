6eb181a3677ded11e46ec4597bc6230c4f9642a4
Antonio Lingardsson Luna <toni.lingardsson@gmail.com>
2024-09-03 14:28:30 +0200

    Create VotingSystem basic structure

---
+++ contracts/VotingSystem.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+contract VotingSystem {
+        // Define the structures, variables, and mappings here
+
+    // Constructor
+    constructor() {
+        // Initialize state variables if needed
+    }
+
+    // Define functions to create poll, start poll, cast vote, count votes, and get winner here
+}
