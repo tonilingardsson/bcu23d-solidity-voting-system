b3aec588c797881b1a9ac64b8d14eaa74ee222dc
Antonio Lingardsson Luna <toni.lingardsson@gmail.com>
2024-09-03 17:35:21 +0200

    Create a Poll Function

--- contracts/VotingSystem.sol
+++ contracts/VotingSystem.sol
@@ -2,22 +2,26 @@
 pragma solidity ^0.8.0;
 
 contract VotingSystem {
-        // Define the structures, variables, and mappings here
-        struct Poll {
-            address creator;
-            string[] recordList;
-            uint startTime;
-            uint endTime;
-            mapping (string => uint) votes;
-            bool isPollActive;
-            bool isPollEnded;
-        }
+    // Define the structures, variables, and mappings here
+    struct Poll {
+        address creator;
+        string[] recordList;
+        uint startTime;
+        uint endTime;
+        mapping(string => uint) votes;
+        bool isPollActive;
+        bool isPollEnded;
+    }
 
-uint public pollCount = 0;
-mapping(uint => Poll) public polls;
-mapping (address => mapping(uint => bool)) public hasVoted;
+    uint public pollCount = 0;
+    mapping(uint => Poll) public polls;
+    mapping(address => mapping(uint => bool)) public hasVoted;
 
-enum PollStatus { NotStarted, Ongoing, Ended}
+    enum PollStatus {
+        NotStarted,
+        Ongoing,
+        Ended
+    }
 
     // Constructor
     constructor() {
@@ -25,4 +29,19 @@ enum PollStatus { NotStarted, Ongoing, Ended}
     }
 
     // Define functions to create poll, start poll, cast vote, count votes, and get winner here
+    function createPoll(
+        string[] memory _recordList,
+        uint _votingDuration
+    ) public {
+        pollCount++;
+        Poll storage newPoll = polls[pollCount];
+        newPoll.creator = msg.sender;
+        newPoll.recordList = _recordList;
+        newPoll.startTime = block.timestamp;
+        newPoll.endTime = newPoll.startTime + _votingDuration;
+        newPoll.isPollActive = true;
+        newPoll.isPollEnded = false;
+
+        emit PollCreated(pollCount, msg.sender);
+    }
 }
